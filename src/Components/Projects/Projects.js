eb50ccae2ceb2b16e3f76d955e92379b62efde27
sabbirquince <59896171+sabbirquince@users.noreply.github.com>
2021-05-04 18:54:42 +0600

    debugged

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Projects.module.css";
 import projectInfo from "./ProjectInfo.json";
 import ProjectCard from "./ProjectCard/ProjectCard";
 import Zoom from "react-reveal/Zoom";
 
 const Projects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    setProjects(projectInfo);
+  }, []);
+
   return (
     <div className={styles.projects}>
       <Zoom cascade>
@@ -12,7 +18,7 @@ const Projects = () => {
       </Zoom>
 
       <section className={styles.cardBox}>
-        {projectInfo.map((project, index) => (
+        {projects.map((project, index) => (
           <ProjectCard key={index} project={project} index={index} />
         ))}
       </section>
