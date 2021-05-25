44ebd2a63d05d7aa6c2ece2458b518419ffcf2bc
sabbirquince <59896171+sabbirquince@users.noreply.github.com>
2021-05-26 00:41:25 +0600

    removed animation

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,11 +4,15 @@ import Navigation from "./Navbar/Navigation";
 import Head from "./Head/Head";
 import Intro from "./Intro/Intro";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import About from "../About/About";
 import Footer from "../Footer/Footer";
-import Projects from "../Projects/Projects";
-import Blogs from "../Blogs/Blogs";
-import Contact from "../Contacts/Contact";
+// import About from "../About/About";
+// import Projects from "../Projects/Projects";
+// import Blogs from "../Blogs/Blogs";
+// import Contact from "../Contacts/Contact";
+const About = React.lazy(() => import("../About/About"));
+const Projects = React.lazy(() => import("../Projects/Projects"));
+const Blogs = React.lazy(() => import("../Blogs/Blogs"));
+const Contact = React.lazy(() => import("../Contacts/Contact"));
 
 const Header = () => {
   const [active, setActive] = useState(1);
